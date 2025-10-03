8f367e776ec3a1727fa5dbfa0cceaf468a28be60
Nikita-Schenikov <nikita-shenikov@mail.ru>
2025-10-03 22:36:53 +0300

    GET and POST working

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+//sources used
+//1) https://purpleschool.ru/knowledge-base/article/creating-rest-api
+// 2) https://dev.to/kengowada/go-routing-101-handling-and-grouping-routes-with-nethttp-4k0e
+
 package main
 
 import (
@@ -7,24 +11,40 @@ import (
     "net/http"
 )
 
-type Response struct {
-    Message string `json:"message"`
+
+type Task struct {
+    ID    string `json:"id"`
+    Text  string `json:"text"`
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+var tasks = []Task{}
+
+func getTasksHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(Response{Message: "Добро пожаловать в API"})
+    json.NewEncoder(w).Encode(tasks)
+}
+
+func createTaskHandler(w http.ResponseWriter, r *http.Request) {
+    var task Task
+    json.NewDecoder(r.Body).Decode(&task)
+    task.ID = fmt.Sprintf("%d", len(tasks)+1)
+    tasks = append(tasks, task)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(task)
 }
 
 func main() {
 
 	r := http.NewServeMux()
 
-    r.HandleFunc("GET /", homeHandler)
+    r.HandleFunc("GET /tasks", getTasksHandler)
+	r.HandleFunc("POST /tasks", createTaskHandler)
 
     fmt.Println("Сервер запущен на http://localhost:8080")
-    fmt.Println("Для проверки откройте браузер или используйте curl:")
-    fmt.Println("curl http://localhost:8080/")
+    fmt.Println("Для проверки откройте браузер или используйте curl http://localhost:8080/tasks")
+	fmt.Println(`Для добавления задачи curl -X POST http://localhost:8080/tasks -H "Content-Type: application/json" -d '{"text": "Задача"}'`)
     
     log.Fatal(http.ListenAndServe(":8080", r))
 }
