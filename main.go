4652079ee42dae3265b3273890ae633bbd579d70
Nikita-Schenikov <nikita-shenikov@mail.ru>
2025-10-05 20:02:45 +0300

    GET tasks from PostgreSQL working

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
     "encoding/json"
     "fmt"
     "log"
-	"strings"
-	"strconv"
+	// "strings"
+	// "strconv"
     "net/http"
     "database/sql"
 
@@ -22,117 +22,148 @@ type Task struct {
     Text  string `json:"text"`
 }
 
-var tasks = []Task{}
+var db *sql.DB
 
-func getTasksHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(tasks)
+func initDB() {
+    connStr := "postgresql://postgres:4840707101@localhost:8000/gosprints?sslmode=disable"
+    var err error
+    db, err = sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal("Error opening database:", err)
+    }
+
+    if err = db.Ping(); err != nil {
+        log.Fatal("Error connecting to database:", err)
+    }
+    fmt.Println("Connected to PostgreSQL!")
 }
 
-func writingTaskHandler(w http.ResponseWriter, r *http.Request) {
+func getTasksHandler(db *sql.DB) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        
+        if r.Method != "GET" {
+            http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+            return
+        }
+        
+        rows, err := db.Query("SELECT id, text FROM \"Tasks\"") //уточняем что таблица называется Tasks
+        if err != nil {
+            http.Error(w, "Database error", http.StatusInternalServerError)
+            return
+        }
+        defer rows.Close()
 
-	path := strings.TrimPrefix(r.URL.Path, "/tasks/")
-    idStr := strings.Split(path, "/")[0] // способ достать id из эндпоинта
-	id, _ := strconv.Atoi(idStr) //переводим string в int игнорируя второй параметр Atoi
-
-    for i, task := range tasks {
-		idx, _ := strconv.Atoi(task.ID)
-        if idx == id {
-            json.NewDecoder(r.Body).Decode(&tasks[i])
-            w.Header().Set("Content-Type", "application/json")
-            json.NewEncoder(w).Encode(tasks[i])
+        var tasks []Task
+        for rows.Next() {
+            var task Task
+            if err := rows.Scan(&task.ID, &task.Text); err != nil {
+                http.Error(w, "Database error", http.StatusInternalServerError)
+                return
+            }
+            tasks = append(tasks, task)
+        }
+        
+        if err = rows.Err(); err != nil {
+            http.Error(w, "Database error", http.StatusInternalServerError)
+            return
+        }
+        
+        w.Header().Set("Content-Type", "application/json")
+        if err := json.NewEncoder(w).Encode(tasks); err != nil {
             return
         }
     }
-
-    http.Error(w, "Задача с указанным id не найдена", http.StatusNotFound)
 }
 
-func createTaskHandler(w http.ResponseWriter, r *http.Request) {
-    var task Task
-    json.NewDecoder(r.Body).Decode(&task)
-    task.ID = fmt.Sprintf("%d", len(tasks)+1)
-    tasks = append(tasks, task)
+// func writingTaskHandler(w http.ResponseWriter, r *http.Request) {
 
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(task)
-}
+// 	path := strings.TrimPrefix(r.URL.Path, "/tasks/")
+//     idStr := strings.Split(path, "/")[0] // способ достать id из эндпоинта
+// 	id, _ := strconv.Atoi(idStr) //переводим string в int игнорируя второй параметр Atoi
 
-func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
+//     for i, task := range tasks {
+// 		idx, _ := strconv.Atoi(task.ID)
+//         if idx == id {
+//             json.NewDecoder(r.Body).Decode(&tasks[i])
+//             w.Header().Set("Content-Type", "application/json")
+//             json.NewEncoder(w).Encode(tasks[i])
+//             return
+//         }
+//     }
 
-	path := strings.TrimPrefix(r.URL.Path, "/tasks/")
-    idStr := strings.Split(path, "/")[0] 
-	id, _ := strconv.Atoi(idStr) 
+//     http.Error(w, "Задача с указанным id не найдена", http.StatusNotFound)
+// }
 
-	var updatedTask Task
-	json.NewDecoder(r.Body).Decode(&updatedTask)
+// func createTaskHandler(w http.ResponseWriter, r *http.Request) {
+//     var task Task
+//     json.NewDecoder(r.Body).Decode(&task)
+//     task.ID = fmt.Sprintf("%d", len(tasks)+1)
+//     tasks = append(tasks, task)
 
-    for i, task := range tasks {
-		idx, _ := strconv.Atoi(task.ID)
-        if idx == id {
-            tasks[i].Text = updatedTask.Text
-            w.Header().Set("Content-Type", "application/json")
-            json.NewEncoder(w).Encode(tasks[i])
-            return
-        }
-    }
+//     w.Header().Set("Content-Type", "application/json")
+//     w.WriteHeader(http.StatusCreated)
+//     json.NewEncoder(w).Encode(task)
+// }
 
-    http.Error(w, "Задача с указанным id не найдена", http.StatusNotFound)
-}
+// func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
 
-func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+// 	path := strings.TrimPrefix(r.URL.Path, "/tasks/")
+//     idStr := strings.Split(path, "/")[0] 
+// 	id, _ := strconv.Atoi(idStr) 
 
-	path := strings.TrimPrefix(r.URL.Path, "/tasks/")
-    idStr := strings.Split(path, "/")[0]
-	id, _ := strconv.Atoi(idStr)
+// 	var updatedTask Task
+// 	json.NewDecoder(r.Body).Decode(&updatedTask)
 
-    for i, task := range tasks {
-		idx, _ := strconv.Atoi(task.ID)
-        if idx == id {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-            w.WriteHeader(http.StatusNoContent)
-            return
-        }
-    }
+//     for i, task := range tasks {
+// 		idx, _ := strconv.Atoi(task.ID)
+//         if idx == id {
+//             tasks[i].Text = updatedTask.Text
+//             w.Header().Set("Content-Type", "application/json")
+//             json.NewEncoder(w).Encode(tasks[i])
+//             return
+//         }
+//     }
 
-    http.Error(w, "Задача с указанным id не найдена", http.StatusNotFound)
-}
+//     http.Error(w, "Задача с указанным id не найдена", http.StatusNotFound)
+// }
+
+// func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+// 	path := strings.TrimPrefix(r.URL.Path, "/tasks/")
+//     idStr := strings.Split(path, "/")[0]
+// 	id, _ := strconv.Atoi(idStr)
+
+//     for i, task := range tasks {
+// 		idx, _ := strconv.Atoi(task.ID)
+//         if idx == id {
+// 			tasks = append(tasks[:i], tasks[i+1:]...)
+//             w.WriteHeader(http.StatusNoContent)
+//             return
+//         }
+//     }
+
+//     http.Error(w, "Задача с указанным id не найдена", http.StatusNotFound)
+// }
 
 func main() {
 
-    connStr := "postgresql://postgres:4840707101@localhost:8000/gosprints?sslmode=disable"
-
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		fmt.Println("Ошибка открытия соединения:", err)
-		return
-	}
-	defer db.Close()
-
-	// Проверка соединения
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("Ошибка пинга базы данных:", err)
-		return
-	}
-
-	fmt.Println("Успешное подключение к PostgreSQL!")
+    initDB()
+    defer db.Close()
 
 	r := http.NewServeMux()
 
-    r.HandleFunc("GET /tasks", getTasksHandler)
-	r.HandleFunc("POST /tasks", createTaskHandler)
-	r.HandleFunc("GET /tasks/{id}", writingTaskHandler)
-	r.HandleFunc("PUT /tasks/{id}", updateTaskHandler)
-	r.HandleFunc("DELETE /tasks/{id}", deleteTaskHandler)
+    r.HandleFunc("GET /tasks", getTasksHandler(db))
+	// r.HandleFunc("POST /tasks", createTaskHandler)
+	// r.HandleFunc("GET /tasks/{id}", writingTaskHandler)
+	// r.HandleFunc("PUT /tasks/{id}", updateTaskHandler)
+	// r.HandleFunc("DELETE /tasks/{id}", deleteTaskHandler)
 
     fmt.Println("Сервер запущен на http://localhost:8080")
     fmt.Println("Для проверки откройте браузер или используйте curl http://localhost:8080/tasks")
-	fmt.Println(`Для добавления задачи curl -X POST http://localhost:8080/tasks -H "Content-Type: application/json" -d '{"text": "Задача"}'`)
-	fmt.Println("Для чтения задачи curl http://localhost:8080/tasks/{id}")
-	fmt.Println(`Для обновления задачи curl -X PUT http://localhost:8080/tasks/{id} -H "Content-Type: application/json" -d '{"text": "Обновленный текст задачи"}'`)
-	fmt.Println("Для удаления задачи curl -X DELETE http://localhost:8080/tasks/{id}")
+	// fmt.Println(`Для добавления задачи curl -X POST http://localhost:8080/tasks -H "Content-Type: application/json" -d '{"text": "Задача"}'`)
+	// fmt.Println("Для чтения задачи curl http://localhost:8080/tasks/{id}")
+	// fmt.Println(`Для обновления задачи curl -X PUT http://localhost:8080/tasks/{id} -H "Content-Type: application/json" -d '{"text": "Обновленный текст задачи"}'`)
+	// fmt.Println("Для удаления задачи curl -X DELETE http://localhost:8080/tasks/{id}")
     
     log.Fatal(http.ListenAndServe(":8080", r))
 }
